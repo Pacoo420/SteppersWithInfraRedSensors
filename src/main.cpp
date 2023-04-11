29b044c6c076e512bef143a7f07e40f037c05899
Adrian Pacera <i502817@fhict.nl>
2023-04-11 12:59:33 +0200

    "C# controlling homing"

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <Stepper.h>
 
 //X axis sensor 
-#define IR_PIN 32
+#define IR1_PIN 32
+#define IR2_PIN 35
+#define IR3_PIN 34
 
 //Motor for X axis
 #define xIN1 15
@@ -17,21 +19,29 @@
 #define yIN4 19
 
 //Motor for Z axis
-#define zIN1 22
-#define zIN2 1
-#define zIN3 3
-#define zIN4 21
+#define zIN1 21
+#define zIN2 26
+#define zIN3 22
+#define zIN4 23
 
-const int stepPerRevolution = 100;
+const int stepPerRevolution = 2000;
 
 unsigned int timeLoop;
 unsigned int timeStart;
-bool spinMotorX = true;
+bool homing=false;
+
+bool spinMotorX = false;
+bool spinMotorY = false;
+bool spinMotorZ = false;
+
 double distance;
 
 int value;
 int homingState = 0;
 int distance_ir = 0;
+
+int currentMessage;
+int lastMessage;
 
 Stepper motorX(stepPerRevolution, xIN1, xIN3, xIN2, xIN4);
 Stepper motorY(stepPerRevolution, yIN1, yIN3, yIN2, yIN4);
@@ -39,50 +49,93 @@ Stepper motorZ(stepPerRevolution, zIN1, zIN3, zIN2, zIN4);
 
 void setup(){
   //IR pinmode setup
-  pinMode(IR_PIN, INPUT);
+  pinMode(IR1_PIN, INPUT);
+  pinMode(IR2_PIN, INPUT);
+  pinMode(IR3_PIN, INPUT);
 
   //motor speed setup
-  motorX.setSpeed(25);
-  motorY.setSpeed(25);
-  motorZ.setSpeed(25);
+  motorX.setSpeed(5); //was 25
+  motorY.setSpeed(5);
+  motorZ.setSpeed(5);
 
   //debug monitor setup
   Serial.begin(9600);
 }
 
-int IRSensor(){
+int IRSensor(int IR_PIN){
   int sensorValue = analogRead(IR_PIN);
   distance = 12.08 * pow(sensorValue / 1024.0, -1.204);               
-  timeStart = millis();
   return distance;                     
 }
 
 void moveMotorX(){
   while (spinMotorX == true){
-    value = IRSensor();
+    int sensorReading = analogRead(A0);
+    value = IRSensor(IR1_PIN);
     Serial.println(value);
-    motorX.step(stepPerRevolution);
+    motorX.step(stepPerRevolution / 100);
 
     if(value<=10){
       homingState = 1;
       spinMotorX = false;
     }
   }
- }
-void loop(){
-  switch(homingState){
-    case 0:
-      moveMotorX();
-      break;
-    case 1:
-      Serial.println("koniec kurvy");
-      break;
-  }
-
-  //motorZ.step(stepPerRevolution);
-  //motorX.step(stepPerRevolution);
- // motorY.step(stepPerRevolution);
- // delay(1000);
-
 }
 
+void moveMotorY(){
+  while (spinMotorY == true){
+    value = IRSensor(IR2_PIN);
+    Serial.println(value);
+    motorY.step(stepPerRevolution / 100);
+
+    if(value<=10){
+      homingState = 2;
+      spinMotorY = false;
+    }
+  }
+}
+
+void moveMotorZ(){
+  while (spinMotorZ == true){
+    value = IRSensor(IR3_PIN);
+    Serial.println(value);
+    motorZ.step(stepPerRevolution / 100);
+
+    if(value<=10){
+      spinMotorZ = false;
+      homing = false;
+    }
+  }
+}
+
+void readMessage(){
+  currentMessage = Serial.read();
+
+  if(currentMessage == 'Y'){
+    homing = true;
+  }
+}
+
+void motorControl(){
+  while(homing == true){
+    switch(homingState){
+      case 0:
+        spinMotorX = true;
+        moveMotorX();
+        break;
+      case 1:
+        spinMotorY = true;
+        moveMotorY();
+        break;
+      case 2:
+        spinMotorZ = true;
+        moveMotorZ();
+        break; 
+    }
+  }
+}
+
+void loop(){
+  readMessage();
+  motorControl();
+}
